f57295d42a1e038892d25bfa2abeb7f904e96df2
Wtorres <wtorres@cads.com.gt>
2018-08-24 17:24:14 -0600

    Ajustess

--- webStore/src/app/app.component.ts
+++ webStore/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireDatabase } from '../../node_modules/angularfire2/database';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  form: FormGroup;
   title = 'app';
+
+  constructor(private fb: FormBuilder, private af: AngularFireDatabase) {
+    this.createForm();
+  }
+  createForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+  onSubmit() {
+    const {name, email, message} = this.form.value;
+    const date = Date();
+    const html = `
+      <div>From: ${name}</div>
+      <div>Email: <a href="mailto:${email}">${email}</a></div>
+      <div>Date: ${date}</div>
+      <div>Message: ${message}</div>
+    `;
+    let formRequest = { name, email, message, date, html };
+    this.af.database.refFromURL('/messages').push(formRequest);
+    this.form.reset();
+  }
 }
--- webStore/src/app/app.module.ts
+++ webStore/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -48,7 +48,8 @@ import { KardexComponent } from './components/kardex/kardex.component';
     AngularFireDatabaseModule,
     AngularFirestoreModule,
     APP_ROUTES,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     TodoService
